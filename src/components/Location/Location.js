96c18d563a06452b08f04403f6c37750f9ed9725
Kyle Holmberg <kylemh.email@gmail.com>
2017-06-25 12:56:34 -0700

    fix #62 with timeout

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -4,8 +4,9 @@ import { geolocated } from 'react-geolocated';
 
 class Location extends Component {
   componentWillReceiveProps(nextProps) {
-    if (nextProps.isGeolocationAvailable && nextProps.isGeolocationEnabled){
-    this.props.setLocation(nextProps);
+    setTimeout(500);
+    if (nextProps.isGeolocationAvailable && nextProps.isGeolocationEnabled) {
+      this.props.setLocation(nextProps);
     }
   }
 
