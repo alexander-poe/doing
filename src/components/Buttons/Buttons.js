0f9ba829a746ab2a5ce1934d5f0042bfad90bdc9
David Martin <davidjmartin01@gmail.com>
2017-06-25 09:58:45 -0700

    creates dummy button component

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export default () => (
+  <div>
+    <Button bsStyle="primary">Primary</Button>
+    <Button bsStyle="primary">Primary</Button>
+    <Button bsStyle="primary">Primary</Button>
+    <Button bsStyle="primary">Primary</Button>
+  </div>
+);
+
