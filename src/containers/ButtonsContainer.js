f68908982b8a621ed90543a8b11d506792818d63
David Martin <davidjmartin01@gmail.com>
2017-06-25 10:30:35 -0700

    fixes misnamed props

--- src/containers/ButtonsContainer.js
+++ src/containers/ButtonsContainer.js
@@ -5,7 +5,7 @@ import getBar from '../redux/actions/fetchBar';
 import fetchPark from '../redux/actions/fetchPark';
 
 const mapStateToProps = state => ({
-  location: state.location,
+  location: state.handleLocation,
 });
 
 
